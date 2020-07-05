235c2bb0091cc10d86c320acdbe891e3efc9cd51
Maksym Karunos <maksymkarunos@Maksyms-MBP.fios-router.home>
2020-07-05 12:10:34 -0400

    set up the backend

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Task
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта',  'tasks':tasks})
 
 
 def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
     return render(request, 'main/about.html')
